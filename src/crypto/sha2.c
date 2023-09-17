7a4e216bede5fdc43e2b4ae64cc978198b0d41d2
Richard Moore <me@ricmoo.com>
2023-09-17 04:24:54 -0400

    Refactor SHA2 to use shared tables with SHA2-512.

--- src/crypto/sha2.c
+++ src/crypto/sha2.c
@@ -52,7 +52,7 @@ static void reverseBuffer(uint32_t *values, uint_fast8_t length) {
 
 #endif /* LITTLE_ENDIAN */
 
-#define SHA256_BLOCK_LENGTH 64
+//#define SHA256_BLOCK_LENGTH 64
 #define SHA256_DIGEST_LENGTH 32
 #define SHA256_DIGEST_STRING_LENGTH (SHA256_DIGEST_LENGTH * 2 + 1)
 #define SHA256_SHORT_BLOCK_LENGTH (SHA256_BLOCK_LENGTH - 8)
@@ -86,53 +86,60 @@ static void memzero(uint8_t *dst, uint32_t length) {
     */
 }
 
-// Returns true if value is prime
-/*
-uint8_t isPrime(uint16_t value) {
-    if ((value % 2) == 0) { return 0; }
-    for (uint_fast16_t i = 3; i < value; i += 2) {
-        if (value % i == 0) { return 0; }
-    }
-    return 1;
-}
 
-// Returns the index-th prime number
-uint16_t getPrime(uint8_t index) {
-    if (index == 0) { return 2; }
-    if (index == 1) { return 3; }
-
-    index -= 2;
-
-    for (uint_fast16_t i = 6; i <= 312; i += 6) {
-        if (isPrime(i - 1)) {
-            if (index == 0) { return i - 1; }
-            index--;
-        }
-        if (isPrime(i + 1)) {
-            if (index == 0) { return i + 1; }
-            index--;
-        }
-    }
-
-    return 0;
-}
-*/
-/*
-const uint8_t lowPrimes[] = {
-    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
-    71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
-    131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
-    233, 239, 241, 251};
-
-const uint16_t highPrimes[] = {
-    257, 263, 269, 271, 277, 281, 283, 293, 307, 311
+static const uint32_t kHi[] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+    0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+    0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+    0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+    0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+    0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2, 0xca273ece, 0xd186b8c7,
+    0xeada7dd6, 0xf57d4f7f, 0x06f067aa, 0x0a637dc5, 0x113f9804, 0x1b710b35,
+    0x28db77f5, 0x32caab7b, 0x3c9ebe0a, 0x431d67c4, 0x4cc5d4be, 0x597f299c,
+    0x5fcb6fab, 0x6c44198c
 };
 
-static uint16_t getPrime(uint8_t index) {
-    if (index < 54) { return lowPrimes[index]; }
-    return highPrimes[index - 54];
+static const uint32_t kLo[] = {
+     0xd728ae22, 0x23ef65cd, 0xec4d3b2f, 0x8189dbbc, 0xf348b538, 0xb605d019,
+     0xaf194f9b, 0xda6d8118, 0xa3030242, 0x45706fbe, 0x4ee4b28c, 0xd5ffb4e2,
+     0xf27b896f, 0x3b1696b1, 0x25c71235, 0xcf692694, 0x9ef14ad2, 0x384f25e3,
+     0x8b8cd5b5, 0x77ac9c65, 0x592b0275, 0x6ea6e483, 0xbd41fbd4, 0x831153b5,
+     0xee66dfab, 0x2db43210, 0x98fb213f, 0xbeef0ee4, 0x3da88fc2, 0x930aa725,
+     0xe003826f, 0x0a0e6e70, 0x46d22ffc, 0x5c26c926, 0x5ac42aed, 0x9d95b3df,
+     0x8baf63de, 0x3c77b2a8, 0x47edaee6, 0x1482353b, 0x4cf10364, 0xbc423001,
+     0xd0f89791, 0x0654be30, 0xd6ef5218, 0x5565a910, 0x5771202a, 0x32bbd1b8,
+     0xb8d2d0c8, 0x5141ab53, 0xdf8eeb99, 0xe19b48a8, 0xc5c95a63, 0xe3418acb,
+     0x7763e373, 0xd6b2b8a3, 0x5defb2fc, 0x43172f60, 0xa1f0ab72, 0x1a6439ec,
+     0x23631e28, 0xde82bde9, 0xb2c67915, 0xe372532b, 0xea26619c, 0x21c0c207,
+     0xcde0eb1e, 0xee6ed178, 0x72176fba, 0xa2c898a6, 0xbef90dae, 0x131c471b,
+     0x23047d84, 0x40c72493, 0x15c9bebc, 0x9c100d4c, 0xcb3e42b6, 0xfc657e2a,
+     0x3ad6faec, 0x4a475817
+};
+
+static const uint32_t kInitHi[] = {
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+};
+
+static const uint32_t kInitLo[] = {
+    0xf3bcc908, 0x84caa73b, 0xfe94f82b, 0x5f1d36f1,
+    0xade682d1, 0x2b3e6c1f, 0xfb41bd6b, 0x137e2179
+};
+
+static uint32_t getConstant256(uint32_t index, uint32_t init) {
+    if (init) { return kInitHi[index]; }
+    return kHi[index];
 }
-*/
+
+static uint64_t getConstant512(uint32_t index, uint32_t init) {
+    if (init) { return  ((uint64_t)kInitHi[index] << 32) | kInitLo[index]; }
+    return  ((uint64_t)kHi[index] << 32) | kLo[index];;
+}
 
 static const uint32_t K256_2[] = {
     0x6a09e667UL,
@@ -164,27 +171,9 @@ static const uint32_t K256_3[64] = {
 	0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2UL
 };
 
-static uint32_t getConstant(uint8_t primeIndex, uint8_t cubeRoot) {
-    if (cubeRoot) { return K256_3[primeIndex]; }
-    return K256_2[primeIndex];
-}
-/*
-static uint32_t getConstant(uint8_t primeIndex, uint8_t cubeRoot) {
-    double v = getPrime(primeIndex);
-    if (cubeRoot) {
-        v = pow(v, 1.0 / 3.0);
-    } else {
-        v = pow(v, 0.5);
-    }
-    v -= (int)v;
-    v *= 0x100000000;
-    return (uint32_t)v;
-}
-*/
-
 void sha256_init(Sha256Context *context) {
     for (int_fast8_t i = 0; i < 8; i++) {
-        context->state[i] = getConstant(i, 0);
+        context->state[i] = getConstant256(i, 1);
     }
     for (uint_fast8_t i = 0; i < (64 / sizeof(uint32_t)); i++) {
         context->buffer[i] = 0;
@@ -204,7 +193,7 @@ static void sha256_Transform(const uint32_t *state_in, const uint32_t *data, uin
 
     for (uint_fast8_t j = 0; j < 16; j++) {
         /* Apply the SHA-256 compression function to update a..h with copy */
-        T1 = working[7] + Sigma1_256(working[4]) + Ch(working[4], working[5], working[6]) + getConstant(j, 1) + (W256[j] = *data++);
+        T1 = working[7] + Sigma1_256(working[4]) + Ch(working[4], working[5], working[6]) + getConstant256(j, 0) + (W256[j] = *data++);
         T2 = Sigma0_256(working[0]) + Maj(working[0], working[1], working[2]);
         for (int_fast8_t i = 7; i > 0; i--) {
             working[i] = working[i - 1];
@@ -221,7 +210,7 @@ static void sha256_Transform(const uint32_t *state_in, const uint32_t *data, uin
         s1 = sigma1_256(s1);
 
         /* Apply the SHA-256 compression function to update a..h */
-        T1 = working[7] + Sigma1_256(working[4]) + Ch(working[4], working[5], working[6]) + getConstant(j, 1) +
+        T1 = working[7] + Sigma1_256(working[4]) + Ch(working[4], working[5], working[6]) + getConstant256(j, 0) +
              (W256[j & 0x0f] += s1 + W256[(j + 9) & 0x0f] + s0);
         T2 = Sigma0_256(working[0]) + Maj(working[0], working[1], working[2]);
 
@@ -353,11 +342,3 @@ void sha256_final(Sha256Context *context, uint8_t *digest) {
     usedspace = 0;
 }
 
-/*
-void ethers_sha256(uint8_t *data, uint32_t dataLength, uint8_t *digest) {
-    Sha256Context context;
-    ethers_sha256Init(&context);
-    ethers_sha256Update(&context, data, dataLength);
-    ethers_sha256Finalize(&context, digest);
-}
-*/
--- src/crypto/sha2.h
+++ src/crypto/sha2.h
@@ -8,21 +8,51 @@ extern "C" {
 #include <stdint.h>
 
 #define SHA256_DIGEST_SIZE (32)
+#define SHA512_DIGEST_SIZE (64)
+
+
+#define SHA256_BLOCK_LENGTH		(64)
+#define SHA512_BLOCK_LENGTH		(128)
+
 
 typedef struct Sha256Context {
     uint32_t state[8];
     uint32_t bitCount;
-    uint32_t buffer[64 / sizeof(uint32_t)];
+    uint32_t buffer[SHA256_BLOCK_LENGTH / sizeof(uint32_t)];
 } Sha256Context;
 
+typedef struct Sha512Context {
+	uint64_t	state[8];
+	uint64_t	bitcount[2];
+	uint64_t	buffer[SHA512_BLOCK_LENGTH / sizeof(uint64_t)];
+} Sha512Context;
 
-// Streaming SHA2-256 hashing
-void sha256_init(Sha256Context *context);
-void sha256_update(Sha256Context *context, const uint8_t *data, uint32_t dataLength);
-void sha256_final(Sha256Context *context, uint8_t *digest);
+typedef struct HmacSha256Context {
+  uint8_t o_key_pad[SHA256_BLOCK_LENGTH];
+  Sha512Context ctx;
+} HmacSha256Context;
 
-// Single-shot SHA2-256 hashing
-//void ethers_sha256(uint8_t *data, uint32_t dataLength, uint8_t *digest);
+typedef struct HmacSha512Context {
+  uint8_t o_key_pad[SHA512_BLOCK_LENGTH];
+  Sha512Context ctx;
+} HmacSha512Context;
+
+
+void sha2_initSha256(Sha256Context *context);
+void sha2_updateSha256(Sha256Context *context, const uint8_t *data, uint32_t dataLength);
+void sha2_finalSha256(Sha256Context *context, uint8_t *digest);
+
+void sha2_initHmacSha256(HmacSha256Context *context, const uint8_t *key, const uint32_t keylen);
+void sha2_updateHmacSha256(HmacSha256Context *context, const uint8_t *data, const uint32_t dataLength);
+void sha2_finalHmacSha256(HmacSha256Context *context, uint8_t *hmac);
+
+void sha2_initSha512(Sha512Context *context);
+void sha2_updateSha512(Sha512Context *context, const uint8_t *data, uint32_t dataLength);
+void sha2_finalSha512(Sha512Context *context, uint8_t *digest);
+
+void sha2_initHmacSha512(HmacSha512Context *context, const uint8_t *key, const uint32_t keylen);
+void sha2_updateHmacSha512(HmacSha512Context *context, const uint8_t *data, const uint32_t dataLength);
+void sha2_finalHmacSha512(HmacSha512Context *context, uint8_t *hmac);
 
 
 #ifdef __cplusplus
