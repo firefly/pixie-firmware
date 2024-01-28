cfc462d6b10925f513c17a74692373b2781a08b7
Richard Moore <me@ricmoo.com>
2024-01-27 21:44:57 -0500

    Add HSV support to color library.

--- main/system/color.c
+++ main/system/color.c
@@ -1,3 +1,149 @@
+#include <stdio.h>
 
 #include "./color.h"
 
+#define COLOR_HSV          (0x80000000)
+#define NEGATIVE_HUE       (0x40000000)
+
+// Color Format:
+//  - RGBA = 00AA AAAA  RRRR RRRR  GGGG GGGG  BBBB BBBB
+//  - HSVA = 1NAA AAAA  HHHH HHHH  HHHH SSSS  SSVV VVVV   (N => negative hue)
+//  - Alpha is 0 for fully opaque and 0x3f for fully transparent
+
+
+#define CLAMP(v,m)     (((v) < 0) ? 0: ((v) > (m)) ? (m): (v))
+
+color_t color_rgb(int32_t r, int32_t g, int32_t b, int32_t alpha) {
+    color_t result = 0;
+
+    result |= CLAMP(r, 0xff) << 16;
+    result |= CLAMP(g, 0xff) << 8;
+    result |= CLAMP(b, 0xff);
+    result |= (0x3f - CLAMP(alpha, 0x3f)) << 24;
+
+    return result;
+}
+
+color_t color_hsv(int32_t h, int32_t s, int32_t v, int32_t alpha) {
+    color_t result = COLOR_HSV;
+
+    if (h < 0) {
+        result |= NEGATIVE_HUE;
+        h *= -1;
+    }
+
+    result |= CLAMP(h, 0xfff) << 12;
+    result |= CLAMP(s, 0x3f) << 6;
+    result |= CLAMP(v, 0x3f);
+    result |= (0x3f - CLAMP(alpha, 0x3f)) << 24;
+
+    return result;
+}
+
+// Stores RGB565 as a uint16_t
+#define RGB16(r,g,b)     ((((r) & 0xf8) << 8) | (((g) & 0xfc) << 3) | (((b) & 0xf8) >> 3))
+
+// Stores RGB888 as a uint32_t
+#define RGB24(r,g,b)    ((rgb24_t)(((r) << 16) | ((g) << 8) | (b)))
+
+#define B6     (0x3f)
+
+// Heavily inspired by: https://stackoverflow.com/questions/3018313/algorithm-to-convert-rgb-to-hsv-and-hsv-to-rgb-in-range-0-255-for-both
+static color_t _fromHSV(color_t color) {
+
+    int32_t h = ((color >> 12) & 0xfff);
+    int32_t s = (color >> 6) & B6;
+    int32_t v = color & B6;
+    if (color & NEGATIVE_HUE) { h *= -1; }
+
+    // Normalize h to [0, 359]  // @TODO: Scale up!
+    h %= 360;
+    if (h < 0) { h += 360; }
+
+    int32_t region = h / 60;
+    int32_t rem = (h - (region * 60));
+
+    v = (v * 255) / B6;
+
+    // Notes:
+    //  - v, t, p, q MUST all be [0, 255] for the RGB mapping
+    //  - s is [0, 63]
+    //  - rem is [0, 59]
+    //  - v, s and rem represent values [0, 1] multiplied by their multiplier
+    //    - so (63 - s) is the equivlent of (1.0 - s_)
+    //    - (v * s) / (255 * 63) is equivalent of (v_ * s_)
+
+    // Compute the 
+    int32_t p = ((v * (63 - s)) * 255) / (255 * 63);
+    int32_t q = ((v * ((63 * 59) - (s * rem))) * 255) / (255 * 63 * 59);
+    int32_t t = ((v * ((63 * 59) - (s * (59 - rem)))) * 255) / (255 * 63 * 59);
+
+    // Mask out the two HSV-specific bits
+    color_t result = color & 0x3f000000;
+
+    // Map the RGB value based on the region
+    switch (region) {
+        case 0:
+            result |= (v << 16) | (t << 8) | p;
+            break;
+        case 1:
+            result |= (q << 16) | (v << 8) | p;
+            break;
+        case 2:
+            result |= (p << 16) | (v << 8) | t;
+            break;
+        case 3:
+            result |= (p << 16) | (q << 8) | v;
+            break;
+        case 4:
+            result |= (t << 16) | (p << 8) | v;
+            break;
+        case 5:
+            result |= (v << 16) | (p << 8) | q;
+            break;
+    }
+
+    return result;
+}
+
+rgb16_t color_rgb16(color_t color) {
+    if (color & COLOR_HSV) { color = _fromHSV(color); }
+    return RGB16((color >> 24) & 0xff, (color >> 16) & 0xff, color & 0xff);
+}
+
+//rgba16_t color_rgba16(color_t color) {
+//    if (color & COLOR_HSV) { color = _fromHSV(color); }
+//    return RGBA16((color >> 24) & 0xff, (color >> 16) & 0xff, color & 0xff, color >> 24);
+//}
+
+rgb24_t color_rgb24(color_t color) {
+    if (color & COLOR_HSV) { color = _fromHSV(color); }
+    return color;
+}
+
+/*
+// Test program to validate HSV conversion
+int main() {
+    for (uint32_t h  = 0; h < 360; h++) {
+        for (uint32_t s  = 0; s <= 0x3f; s++) {
+            for (uint32_t v = 0; v <= 0x3f; v++) {
+                color_t hsv = color_hsv(h, s, v, 0xff);
+                color_t rgb = _fromHSV(hsv);
+                printf("h=%d, s=%d, v=%d, hsv=%u, rgb=%u\n", h,s, v, hsv, rgb);
+            }
+        }
+    }
+}
+*/
+/* Works, but is not efficient
+
+    int32_t region = h / 60;                             // [0, 5]
+    int32_t rem = (h - (region * 60)) * 255 / 59;       // [0, 255]
+
+    s = (s * 255) / B6;
+    v = (v * 255) / B6;
+
+    uint8_t p = ((v * (0xff - s)) * 255) / 0xfe01;
+    uint8_t q = ((v * (0xff - (((s * rem) * 255) / 0xfe01))) * 255) / 0xfe01;
+    uint8_t t = ((v * (0xff - (((s * (0xff - rem)) * 255) / 0xfe01))) * 255) / 0xfe01;
+*/
--- main/system/color.h
+++ main/system/color.h
@@ -10,19 +10,28 @@ extern "C" {
 #include "./curves.h"
 
 
-// A bit that can be set to indicate a color is HSV888
-#define COLOR_HSV      (0x800000)
-
 typedef uint16_t rgb16_t;
 typedef uint32_t rgba16_t;
 
 typedef uint32_t rgb24_t;
 
-// For RGB, color_t and rgba16_t are identical
+
+// General color format that can be used to indicate RGBA and HSVA.
+// - RGB: 8-bits per color, 6-bits alpha
+// - HSV: hue=[-4095,4095], 6-bits for saturation, value and alpha
 typedef uint32_t color_t;
 
 // Stores RGB565 as a uint16_t
 #define RGB16(r,g,b)     ((((r) & 0xf8) << 8) | (((g) & 0xfc) << 3) | (((b) & 0xf8) >> 3))
+
+// Stores RGB888 as a uint32_t
+#define RGB24(r,g,b)    ((rgb24_t)(((r) << 16) | ((g) << 8) | (b)))
+
+// Extract the alpha of a color_t
+#define ALPHA(c)       (0x3f - (((c) >> 24) & 0x3f))
+#define MAX_ALPHA      (0x3f)
+
+
 
 // Stores the 0ARGB8565 as a uint32_t
 //#define RGBA16(r,g,b,a)  (((uint32_t)RGB16((r),(g),(b))) | ((0xff - ((a) & 0xff)) << 16))
@@ -31,8 +40,6 @@ typedef uint32_t color_t;
 // used for a more optimized algorithm
 //#define RGB_DARK50       (RGBA16(0,0,0,128))
 
-// Stores RGB888 as a uint32_t
-#define RGB24(r,g,b)    ((rgb24_t)(((r) << 16) | ((g) << 8) | (b)))
 
 //color_t colorHSV(int32_t hue, int32_t saturation, int32_t value);
 //color_t colorHSVA(int32_t hue, int32_t saturation, int32_t value, int32_t alpha);
@@ -42,13 +49,23 @@ typedef uint32_t color_t;
 
 //rgb24_t color24HSV(int32_t hue, int32_t saturation, int32_t value);
 
+// Convert an RGBA to a color_t. All values are clamped.
+color_t color_rgb(int32_t red, int32_t gren, int32_t blue, int32_t alpha);
+
+// Convert an HSV to a color_t. All values are clamped; this means hue may
+// result in an earlier rotation.
+color_t color_hsv(int32_t hue, int32_t saturation, int32_t value, int32_t alpha);
+
 // Comvert a color to RGB space
 rgb16_t color_rgb16(color_t color);
 rgba16_t color_rgba16(color_t color);
 rgb24_t color_rgb24(color_t color);
 
-// Linear-interpolate from color a to b at parametric value t.
+// Linear-interpolate from color a to b at parametric value t. When
+// the color-spaces (RGB vs HSV) differ, values are coerced to RGBA.
 color_t color_lerpfx(color_t a, color_t b, fixed_t t);
+
+color_t color_blend(color_t foreground, color_t background);
 
 //color_t color_lerp(color_t a, color_t b, uint32_t top, uint32_t bottom);
 
@@ -59,6 +76,7 @@ color_t color_lerpfx(color_t a, color_t b, fixed_t t);
 
 //rgb24_t color_rgb24Lerp(color_t a, color_t b, uint32_t top, uint32_t bottom);
 //rgb24_t color_rgb24Lerpfx(color_t a, color_t b, fixed_t alpha);
+
 
 #ifdef __cplusplus
 }
