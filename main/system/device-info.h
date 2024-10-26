273c7ca478476e7d1a50b8d6c70fead408de9636
Richard Moore <me@ricmoo.com>
2024-10-26 01:40:01 -0400

    Update device info and signing.

--- main/system/device-info.h
+++ main/system/device-info.h
@@ -8,12 +8,62 @@ extern "C" {
 
 #include <stdint.h>
 
-uint32_t device_init();
+#define VERSION              (0x00000101)
+
+#define MANUFACTURER_NAME    ("Firefly")
+#define DEVICE_NAME          ("Firefly Pixie")
+
+
+typedef enum DeviceStatus {
+    DeviceStatusOk              = 0,
+
+    // Initialization errors
+    DeviceStatusFailed          = -1,
+    DeviceStatusNotInitialized  = -10,
+    DeviceStatusMissingEfuse    = -40,
+    DeviceStatusMissingNvs      = -41,
+    DeviceStatusOutOfMemory     = -50,
+
+    // Operation errors
+    DeviceStatusTruncated       = 10,
+} DeviceStatus;
+
 
 uint32_t device_modelNumber();
 uint32_t device_serialNumber();
 
-uint32_t device_canAttest();
+DeviceStatus device_init();
+
+DeviceStatus device_getModelName(char *output, size_t length);
+
+DeviceStatus device_canAttest();
+
+#define CHALLENGE_LENGTH    (32)
+
+typedef struct DeviceAttestation {
+    // Version; should always be 1 (currently)
+    uint8_t version;
+
+    // A random nonce selected by the device during signing
+    uint8_t nonce[16];
+
+    // A challenge provided by the party requesting attestation
+    uint8_t challenge[CHALLENGE_LENGTH];
+
+    // Device info
+    uint32_t modelNumber;
+    uint32_t serialNumber;
+
+    // The device RSA public key (e=65537)
+    uint8_t pubkeyN[384];
+
+    // Signature provided during provisioning that the given
+    // device information has been authenticated by Firefly
+    uint8_t attestProof[64];
+
+    // The RSA attestion (signature) from this device
+    uint8_t signature[384];
+} DeviceAttestation;
 
 /**
  *  Response:
@@ -29,7 +79,7 @@ uint32_t device_canAttest();
  *  Note:
  *   - all signatures are EIP-2098 compressed signatures
  */
-uint32_t device_attest(uint32_t timestamp, uint32_t challenge, uint8_t *resp);
+uint32_t device_attest(uint8_t *challenge, DeviceAttestation *attest);
 
 
 #ifdef __cplusplus
